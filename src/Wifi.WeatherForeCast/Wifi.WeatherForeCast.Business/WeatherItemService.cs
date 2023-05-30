2a847ad355dc64b75c44a898d741ac2e4f870119
Boban Milojevic <milojevic@stea.at>
2023-05-30 11:41:02 +0200

    Change with Interface

--- src/Wifi.WeatherForeCast/Wifi.WeatherForeCast.Business/WeatherItemService.cs
+++ src/Wifi.WeatherForeCast/Wifi.WeatherForeCast.Business/WeatherItemService.cs
@@ -5,20 +5,20 @@ namespace Wifi.WeatherForeCast.Business;
 
 public class WeatherItemService
 {
-    private WeatherData _weatherData;
+    private IWeatherData _IWeatherData;
 
     public WeatherItemService()
     {
-        _weatherData = new WeatherData(47.23306, 9.6);
+        _IWeatherData = new WeatherData(47.23306, 9.6);
     }
 
     public async Task<IQueryable<WeatherItem>> GetWeatherDataOfRemainingDay()
     {
-        return await _weatherData.GetAllDataForRemainingDay();
+        return await _IWeatherData.GetAllDataForRemainingDay();
     }
 
     public async Task<IQueryable<WeatherItem>> GetWeatherDataOfNDays()
     {
-        return _weatherData.GetAllDataAtHourOfDayForTheNext_n_Days(12, 8);
+        return _IWeatherData.GetAllDataAtHourOfDayForTheNext_n_Days(12, 8);
     }
 }
