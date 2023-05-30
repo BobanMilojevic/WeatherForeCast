39bd3842cfbda1ff0511003f7068efd3d8b2e89b
Matthias Covi <m.covi@cwp.uno>
2023-05-30 21:25:22 +0200

    UiSettings class, JsonRepository, RepositoryTests

---
+++ src/Wifi.WeatherForeCast/Wifi.WeatherForeCast.Model/UiSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.WeatherForeCast.Model
+{
+    public class UiSettings
+    {
+        public bool IsDegree { get; set; }
+        public string Location { get; set; }
+        public int ForecastDays { get; set; }
+        public string Language { get; set; }
+
+        public UiSettings() { }
+
+        public UiSettings(bool isDegree, string location, int forecastDays, string language)
+        {
+            IsDegree = isDegree;
+            Location = location;
+            ForecastDays = forecastDays;
+            Language = language;
+        }
+    }
+}
---
+++ src/Wifi.WeatherForeCast/Wifi.WeatherForeCast.Repositories/JsonRepository.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+using Wifi.WeatherForeCast.Model;
+
+namespace Wifi.WeatherForeCast.Repositories
+{
+    public class JsonRepository
+    {
+        public void SaveSettings(UiSettings settings, string fileName = "c:\\temp\\WeatherForecastSettings.json")
+        {
+
+            if (settings == null || string.IsNullOrEmpty(fileName)) { return; }
+
+            string json = JsonConvert.SerializeObject(settings);
+
+            File.WriteAllText(fileName, json);
+        }
+
+
+        public UiSettings LoadSettings(string fileName = "c:\\temp\\WeatherForecastSettings.json")
+        {
+            string json = File.ReadAllText(fileName);
+
+            if (string.IsNullOrEmpty(json)) { return null; }
+
+            UiSettings settings = new UiSettings();
+            settings = JsonConvert.DeserializeObject<UiSettings>(json);
+            return settings;
+        }
+
+
+
+    }
+}
